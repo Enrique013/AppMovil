eedd6ebe1666a7029feea0fa30fb512474fd5036
Enrique013 <20023tn013@utez.edu.mx>
2025-09-21 13:02:46 -0600

    Practica contador y estructuracion de carpetas

---
+++ historial_contador/lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  List<int> _historial = [];
+
+  bool _mostrarHistorial = false;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _historial.add(_counter);
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      _historial.add(_counter);
+    });
+  }
+
+  Widget _mostrarListaHistorial() {
+    if (_historial.isEmpty) {
+      return const Text("No hay historial todavía.");
+    } else {
+      return Expanded(
+        child: ListView.builder(
+          itemCount: _historial.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('Valor: ${_historial[index]}'),
+            );
+          },
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _mostrarHistorial = !_mostrarHistorial;
+                });
+              },
+              child: Text(_mostrarHistorial ? 'Ocultar historial' : 'Mostrar historial'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _historial.clear();
+                });
+              },
+              child: const Text('Limpiar historial'),
+            ),
+            const SizedBox(height: 20),
+            if (_mostrarHistorial) _mostrarListaHistorial(),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
